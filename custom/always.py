e8e0a5c98d632278567aa18d01cdcac07bed50fa
Roku <rokubop@gmail.com>
2023-06-16 16:35:09 -0500

    add yarn commands

--- custom/always.py
+++ custom/always.py
@@ -4,27 +4,24 @@ mod = Module()
 
 @mod.action_class
 class Actions:
-    def serenade_toggle(self):
-        """Toggle Serenade on or off."""
-        actions.key("alt-z")
-        self.talon_toggle()
+    # def serenade_wake():
+    #     """Serenade on."""
+    #     actions.key("alt-z")
+    #     actions.user.talon_sleep()
 
-    def serenade_wake():
-        """Serenade on."""
-        actions.key("alt-z")
-        actions.user.talon_sleep()
-
-    def serenade_sleep():
-        """Serenade off."""
-        actions.key("alt-z")
+    # def serenade_sleep():
+    #     """Serenade off."""
+    #     actions.key("alt-z")
 
     def talon_sleep():
         """Stop listening for Talon commands."""
-        actions.user.sleep_all()
+        actions.speech.disable()
+        # actions.user.sleep_all()
 
     def talon_wake():
         """Start listening for Talon commands."""
-        actions.user.wake_all()
+        actions.speech.enable()
+        # actions.user.wake_all()
 
     def talon_toggle():
         """Toggle Talon on or off."""
@@ -32,8 +29,17 @@ class Actions:
             actions.user.talon_sleep()
         else:
             actions.user.talon_wake()
+        # actions.speech.toggle()
 
     def toggle_voice():
         """Toggle between Serenade and Talon."""
         actions.key("alt-z")
         actions.speech.toggle()
+    
+    # def serenade_toggle(self):
+    #     """Toggle Serenade on or off."""
+    #     actions.key("alt-z")
+    #     if actions.speech.enabled():
+    #         actions.user.talon_sleep()
+    #     else:
+    #         actions.user.talon_wake()
--- custom/always.talon
+++ custom/always.talon
@@ -1,12 +1,12 @@
 mode: all
 -
-serenade (toggle | dog): user.serenade_toggle()
-serenade (sleep | off): user.serenade_sleep()
-serenade (wake | on): user.serenade_wake()
+key(ctrl-f1): user.talon_toggle()
+# serenade (toggle | dog): user.serenade_toggle()
+# serenade (sleep | off): user.serenade_sleep()
+# serenade (wake | on): user.serenade_wake()
 talon (wake | on ): user.talon_wake()
 talon (sleep | off): user.talon_sleep()
 talon (toggle | dog): user.talon_toggle()
 toggle voice: user.toggle_voice()
 voice (toggle | dog): user.toggle_voice()
-key(ctrl-f1): user.talon_toggle()
 go to sleep: user.talon_sleep()
