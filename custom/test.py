9245e45ae1a1607488953e4a96e9024141540cdb
Roku <rokubop@gmail.com>
2023-06-18 12:25:24 -0500

    misc

--- custom/test.py
+++ custom/test.py
@@ -5,27 +5,17 @@ from talon import Module, actions, app, clip, cron, ctrl, imgui, noise, ui, Cont
 from talon_plugins import eye_mouse, eye_zoom_mouse
 from talon_plugins.eye_mouse import config, toggle_camera_overlay, toggle_control
 
-
 mod = Module()
 
-    
-# def fibonacci(n):
-#     while n > 0:
-        # I want to recursively add the stuff
-        # fibonacci(n - 1)
+def fibonacci(n: str):
+    if n == 'world':
+        print('yo')
+
 
 @mod.action_class
+
 class Actions:
     def test_one(whatever: str):
         """test"""
         if whatever == 'world':
             print('yo')
-
-
-
-      
-            
-      
-            
-            
-    
