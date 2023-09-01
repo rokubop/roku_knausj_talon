dd71c6a939dac6e2af5870c7d0f73ef0f52a57ba
Roku <rokubop@gmail.com>
2023-08-31 23:28:49 -0500

    refactor parrot mouse nav mode

--- core/modes/parrot_mode/parrot_mouse_nav_mode.py
+++ core/modes/parrot_mode/parrot_mouse_nav_mode.py
@@ -6,98 +6,66 @@ ctx = Context()
 
 nav_job = None
 direction = None
-default_speed = 3
-slow_speed = 1
-fast_speed = 6
-speed = default_speed
 
-def set_slow_speed():
-    global speed
-    speed = slow_speed
+speeds = {"slow": 1, "default": 4, "fast": 8}
+speed = speeds["default"]
 
-def set_default_speed():
+def update_speed(new_speed):
     global speed
-    speed = default_speed
-
-def set_fast_speed():
-    global speed
-    speed = fast_speed
+    speed = speeds.get(new_speed, speeds["default"])
 
 def nav_tick():
     global direction
-    x, y = ctrl.mouse_pos()
-    if direction == "left":
-        ctrl.mouse_move(x - speed, y)
-    elif direction == "right":
-        ctrl.mouse_move(x + speed, y)
-    elif direction == "up":
-        ctrl.mouse_move(x, y - speed)
-    elif direction == "down":
-        ctrl.mouse_move(x, y + speed)
+    if direction:
+        x, y = ctrl.mouse_pos()
+        dx, dy = direction
+        ctrl.mouse_move(x + dx * speed, y + dy * speed)
+
+def start_moving(dx, dy):
+    global nav_job, direction
+    if nav_job:
+        cron.cancel(nav_job)
+    direction = (dx, dy)
+    nav_job = cron.interval("16ms", nav_tick)
 
 @mod.action_class
 class ParrotMouseNavModeActions:
     def parrot_mouse_nav_move_left():
         """Start moving mouse to the left"""
         print("Start moving mouse to the left")
-        global nav_job, direction
-        if nav_job:
-            cron.cancel(nav_job)
-        direction = "left"
-        nav_job = cron.interval("16ms", nav_tick)
+        start_moving(-1, 0)
 
     def parrot_mouse_nav_move_right():
         """Start moving mouse to the right"""
         print("Start moving mouse to the right")
-        global nav_job, direction
-        if nav_job:
-            cron.cancel(nav_job)
-        direction = "right"
-        nav_job = cron.interval("16ms", nav_tick)
+        start_moving(1, 0)
 
     def parrot_mouse_nav_move_down():
         """Start moving mouse down"""
-        print("Start moving mouse to the down")
-        global nav_job, direction
-        if nav_job:
-            cron.cancel(nav_job)
-        direction = "down"
-        nav_job = cron.interval("16ms", nav_tick)
+        print("Start moving mouse down")
+        start_moving(0, 1)
 
     def parrot_mouse_nav_move_up():
         """Start moving mouse up"""
-        print("Start moving mouse to the up")
-        global nav_job, direction
-        if nav_job:
-            cron.cancel(nav_job)
-        direction = "up"
-        nav_job = cron.interval("16ms", nav_tick)
+        print("Start moving mouse up")
+        start_moving(0, -1)
 
     def parrot_mouse_nav_move_slow():
-        """Move the mouse slowly"""
-        global speed, fast_speed
-        if speed == fast_speed:
-            set_default_speed()
-        else:
-            set_slow_speed()
+        """Move mouse slowly"""
+        update_speed("slow" if speed != speeds["fast"] else "default")
 
     def parrot_mouse_nav_move_fast():
-        """Move the mouse fast"""
-        global speed, slow_speed
-        if speed == slow_speed:
-            set_default_speed()
-        else:
-            set_fast_speed()
+        """Move mouse quickly"""
+        update_speed("fast" if speed != speeds["slow"] else "default")
 
     def parrot_mouse_nav_stop():
-        """Stop moving the mouse"""
+        """Stop moving mouse"""
         print("parrot mouse nav stop")
         global nav_job, direction
         if nav_job:
             cron.cancel(nav_job)
         direction = None
 
-mod = Module()
 @mod.action_class
 class UserActions:
     def parrot_mouse_nav_mode_enable():
@@ -105,23 +73,18 @@ class UserActions:
         print("parrot mouse nav mode enabled")
         actions.user.add_yellow_cursor()
         actions.user.parrot_mouse_nav_stop()
-        set_default_speed()
+        update_speed("default")
         actions.mode.disable("user.parrot")
         actions.mode.disable("command")
         actions.mode.disable("dictation")
         actions.mode.enable("user.parrot_mouse_nav")
 
     def parrot_mouse_nav_mode_disable():
-        """Disable parrot nav mode"""
-        print('parrot mouse nav mode disabled')
+        """Disable parrot mouse nav mode"""
+        print("parrot mouse nav mode disabled")
         actions.user.parrot_mouse_nav_stop()
         actions.user.clear_screen_regions()
-        set_default_speed()
+        update_speed("default")
         actions.mode.disable("user.parrot_mouse_nav")
         actions.mode.disable("command")
         actions.mode.disable("dictation")
-
-@ctx.action_class("user")
-class UserActions:
-    def virtual_region_one():
-        print('got it')
