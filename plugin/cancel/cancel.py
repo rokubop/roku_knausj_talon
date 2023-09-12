d50193a59eaad0cdb84552d940c92e6094e3ace7
Roku <rokubop@gmail.com>
2023-09-11 21:30:14 -0500

    use homophones for cancel

--- plugin/cancel/cancel.py
+++ plugin/cancel/cancel.py
@@ -3,16 +3,16 @@
 from talon import actions, speech_system
 
 # To change the phrase used to cancel commands, you must also adjust plugin/cancel/cancel.talon
-cancel_phrases = ["cancel cancel"]
+# cancel_phrases = ["cancel cancel"]
 
 # Alternatively, if you want to use a two-word phrase that
 # can include homophones, you can use this code instead:
-# cancel_words = ["cancel", "counsel", "council"]
-# cancel_phrases = [
-#     " ".join([word1, word2])
-#     for word1 in cancel_words
-#     for word2 in cancel_words
-# ]
+cancel_words = ["cancel", "counsel", "council"]
+cancel_phrases = [
+    " ".join([word1, word2])
+    for word1 in cancel_words
+    for word2 in cancel_words
+]
 
 def pre_phrase(d):
     if "text" in d and "parsed" in d:
