9c5161d5d34301d29ffd9d5bf5c46c24491e19f6
roku <118577417+rokubop@users.noreply.github.com>
2023-12-26 01:12:30 -0600

    ideas

---
+++ games/factory_game/satisfactory_parrot.py
@@ -0,0 +1,24 @@
+# Menu
+# a drag
+# o right
+# pop click
+# n click
+@continuoustrigger
+# SS scroll Down
+# sh scroll up
+# cluck exit
+# Guh Ctrl
+# t shift
+# tut alt
+# er rpg
+# eh pos
+# ee stop
+# palate repeat
+# @combo("1s")
+# ee SS - normal scroll mode enable
+# tut SS - enable alt scroll
+# t SS - enable shift scroll
+# t eh - toggle active mouse
+# t cluck - toggle menu to game
+# t palate - toggle repeat or macro
+# Guh palate - record macro and set to palate
