bc4f196dad8d9b2bfb1bf638a4736c346f3b4433
Roku <rokubop@gmail.com>
2024-03-17 23:52:04 -0500

    cleanup code

--- experimental/parrot_mode_v5/parrot_utils.py
+++ experimental/parrot_mode_v5/parrot_utils.py
@@ -2,20 +2,6 @@ from talon import Module, actions, cron
 
 mod = Module()
 
-combo_chain = ""
-combo_job = None
-
-def no_op():
-    pass
-
-def parrot_combo_stop():
-    global combo_chain, combo_job
-    if combo_job:
-        cron.cancel(combo_job)
-        combo_job = None
-    actions.user.on_parrot_combo_stop({"combo": combo_chain})
-    combo_chain = ""
-
 def categorize_commands(commands):
     def is_prefix_of_other(cmd):
         return any(other_cmd.startswith(f"{cmd} ") and other_cmd != cmd for other_cmd in commands)
@@ -76,6 +62,7 @@ class ParrotConfig():
                 actions.sleep("20ms")
             self.immediate_commands[sound][1]()
 
+# todo: try using the user's direct reference instead
 parrot_config_saved = ParrotConfig()
 
 @mod.action_class
@@ -111,22 +98,3 @@ class Actions:
     def parrot_config_hide_commands():
         """Hide the commands for the current parrot mode"""
         actions.user.ui_textarea_hide()
-
-    def parrot_combo(name: str):
-        """Helper for combos. Define `ctx` callbacks `on_parrot_combo(ev: dict)` and/or `on_parrot_combo_stop(ev: dict)`"""
-        global combo_chain, combo_job
-        if combo_job:
-            cron.cancel(combo_job)
-
-        combo_chain = combo_chain + f" {name}" if combo_chain else name
-        actions.user.on_parrot_combo({"combo": combo_chain})
-        # parrot_combo_stop()
-        combo_job = cron.after("300ms", parrot_combo_stop)
-
-    def on_parrot_combo(ev: dict):
-        """Called on each individual combo action"""
-        no_op()
-
-    def on_parrot_combo_stop(ev: dict):
-        """Called when a complete combo is finished"""
-        no_op()
